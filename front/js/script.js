debe418ba203586a30fd484c49d6f40ae15a6893
Samsky-png <oussama.nouma1@gmail.com>
2022-06-06 12:33:48 +0200

    changing the .map() into a for()

--- front/js/script.js
+++ front/js/script.js
@@ -2,24 +2,19 @@ const url = "http://localhost:3000/api/products/";
 const itemsId = document.getElementById("items")
 fetch (url)
 
-.then(function(res){
-    if (res.ok){
-        return res.json();
-    }
-})
-.then(function(data) {
-    itemsId.innerHTML = data.map(
-        (items) =>
-    `
-    <a href="./product.html?${items._id}">
+.then( (res) => res.json())
+.then((data) => {
+
+  for (let i = 0 ; i < data.length; i++){
+    itemsId.innerHTML += `
+    <a href="./product.html?${data[i]._id}">
     <article>
-      <img src="${items.imageUrl}" alt="${items.altTxt}">
-      <h3 class="productName">${items.name}</h3>
-      <p class="productDescription">${items.description}</p>
+      <img src="${data[i].imageUrl}" alt="${data[i].altTxt}">
+      <h3 class="productName">${data[i].name}</h3>
+      <p class="productDescription">${data[i].description}</p>
     </article>
-  </a> `     
-    ).join("") ;
-  
+  </a> `;  
+  };
 })
 .catch(function(err){
    alert("Une erreur c'est produite")
