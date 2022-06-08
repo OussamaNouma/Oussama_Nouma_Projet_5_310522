2aa1352c2f2dffef55305217db5e0a06d43c5008
Samsky-png <oussama.nouma1@gmail.com>
2022-06-08 18:30:39 +0200

    adding a function that send key information of the product to the local storage

--- front/js/product.js
+++ front/js/product.js
@@ -1,6 +1,11 @@
+// variables
+const addToCart = document.getElementById("addToCart")
+
+const quantityValue = document.getElementById("quantity");
+
 const urlId  = window.location.search.split("?").join("");
 
-const allColors = document.getElementById("colors");
+const colorValue = document.getElementById("colors");
 
 const title = document.getElementById("title");
 
@@ -8,13 +13,18 @@ const description = document.getElementById("description");
 
 const image = document.getElementsByClassName("item__img")[0];
 
-console.log(document.getElementsByClassName("item__img"))
-
 const price = document.getElementById("price");
 
 const productLink = `http://localhost:3000/api/products/${urlId}`;
+// méthode d'enregistrement d'un objet dans le local storage 
 
-
+Storage.prototype.setObj = function (key, obj) {
+    return this.setItem(key, JSON.stringify(obj))
+}
+Storage.prototype.getObj = function (key) {
+    return JSON.parse(this.getItem(key))
+}
+//récupération de l'id depuis le lien et ainsi implémenter le html
 
 fetch(productLink)
 .then((res) => res.json())
@@ -29,17 +39,47 @@ price.innerText= productData.price;
 
 image.innerHTML = `<img src="${productData.imageUrl}" alt="${productData.altTxt}">`
 
+// Boucle qui va parcourir chaque valeur de Colors dans l'api et les ajouter aux options.
 for (color of productData.colors){
 
-    allColors.innerHTML += 
+    colorValue.innerHTML += 
     `<option value="${color}">${color}</option>`;
-}
+    }
+// Fonction qui va gérer ce qu'on va envoyer au local storage
 
+ function cartItems (urlId, quantity , color){
+    let cart = [];
+
+     if (!localStorage.getObj("cart")) {
+        localStorage.setObj("cart", cart);
+    } else {
+        cart = localStorage.getObj("cart");
+    }
+    quantity = parseInt(quantity);  
+    // vérifier que le produit se trouve dans le panier et agir en conséquence.
+    if (quantity > 0 && color.length > 0){
+        let alreadyInCart = false ;
+        for (let i = 0; i < cart.length; i++) {
+           if (cart[i].urlId == urlId && cart[i].color == color) {
+               cart[i].quantity += quantity;
+               alreadyInCart = true;
+           }            
+        }
+
+        if (alreadyInCart == false) {
+                cart.push({"urlId": urlId, "quantity": quantity, "color": color});
+            }
+            // On gère l'envoi de notre tableau "cart" dans le local storage 
+            localStorage.setObj("cart",cart);
+    }
+    else{
+        alert("Veuillez saisir des valeurs valide")
+    }
+   
+    } 
+    // Envoient de nos données au local storage
+    addToCart.addEventListener("click", event => {
+        cartItems(productData._id, quantityValue.value, colorValue.value);
+        
+    });
 })
-.catch(function(err){
-    alert("Une erreur c'est produite")
- })
-
-
-console.log(allColors)
-     
