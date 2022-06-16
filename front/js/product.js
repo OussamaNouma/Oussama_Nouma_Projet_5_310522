2fbec338e9d5b4cda8276da2fd351715ca861468
Samsky-png <oussama.nouma1@gmail.com>
2022-06-16 20:03:15 +0200

    correcting the indentation

--- front/js/product.js
+++ front/js/product.js
@@ -1,9 +1,9 @@
 // variables
-const addToCart = document.getElementById("addToCart")
+const addToCart = document.getElementById("addToCart");
 
 const quantityValue = document.getElementById("quantity");
 
-const urlId  = window.location.search.split("?").join("");
+const urlId = window.location.search.split("?").join("");
 
 const colorValue = document.getElementById("colors");
 
@@ -16,70 +16,65 @@ const image = document.getElementsByClassName("item__img")[0];
 const price = document.getElementById("price");
 
 const productLink = `http://localhost:3000/api/products/${urlId}`;
-// méthode d'enregistrement d'un objet dans le local storage 
+// méthode d'enregistrement d'un objet dans le local storage
 
 Storage.prototype.setObj = function (key, obj) {
-    return this.setItem(key, JSON.stringify(obj))
-}
+  return this.setItem(key, JSON.stringify(obj));
+};
 Storage.prototype.getObj = function (key) {
-    return JSON.parse(this.getItem(key))
-}
-//récupération de l'id depuis le lien et ainsi implémenter le html
+  return JSON.parse(this.getItem(key));
+};
+//récupération de l'id depuis le lien et ainsi implémenter le html.
 
 fetch(productLink)
-.then((res) => res.json())
+  .then((res) => res.json())
 
-.then((productData) => {
+  .then((productData) => {
+    title.innerText = productData.name;
 
-title.innerText = productData.name;
+    description.innerText = productData.description;
 
-description.innerText = productData.description;
+    price.innerText = productData.price;
 
-price.innerText= productData.price;
+    image.innerHTML = `<img src="${productData.imageUrl}" alt="${productData.altTxt}">`;
 
-image.innerHTML = `<img src="${productData.imageUrl}" alt="${productData.altTxt}">`
-
-// Boucle qui va parcourir chaque valeur de Colors dans l'api et les ajouter aux options.
-for (color of productData.colors){
-
-    colorValue.innerHTML += 
-    `<option value="${color}">${color}</option>`;
+    // Boucle qui va parcourir chaque valeur de Colors dans l'api et les ajouter aux options.
+    for (color of productData.colors) {
+      colorValue.innerHTML += `<option value="${color}">${color}</option>`;
     }
-// Fonction qui va gérer ce qu'on va envoyer au local storage
+    // Fonction qui va gérer ce qu'on va envoyer au local storage
 
- function cartItems (urlId, quantity , color){
-    let cart = [];
+    function cartItems(urlId, quantity, color) {
+      let cart = [];
 
-     if (!localStorage.getObj("cart")) {
+      if (!localStorage.getObj("cart")) {
         localStorage.setObj("cart", cart);
-    } else {
+      } else {
         cart = localStorage.getObj("cart");
-    }
-    quantity = parseInt(quantity);  
-    // vérifier que le produit se trouve dans le panier et agir en conséquence.
-    if (quantity > 0 && color.length > 0){
-        let alreadyInCart = false ;
+      }
+      quantity = parseInt(quantity);
+      // vérifier que le produit se trouve dans le panier et agir en conséquence.
+      if (quantity > 0 && color.length > 0) {
+        let alreadyInCart = false;
         for (let i = 0; i < cart.length; i++) {
-           if (cart[i].urlId == urlId && cart[i].color == color) {
-               cart[i].quantity += quantity;
-               alreadyInCart = true;
-           }            
+          if (cart[i].urlId == urlId && cart[i].color == color) {
+            cart[i].quantity = cart[i].quantity + quantity;
+            alreadyInCart = true;
+          }
         }
 
         if (alreadyInCart == false) {
-                cart.push({"urlId": urlId, "quantity": quantity, "color": color});
-            }
-            // On gère l'envoi de notre tableau "cart" dans le local storage 
-            localStorage.setObj("cart",cart);
+          cart.push({ urlId: urlId, quantity: quantity, color: color });
+          window.location.href=""
+        }
+        // On gère l'envoi de notre tableau "cart" dans le local storage
+        localStorage.setObj("cart", cart);
+      } else {
+        alert("Veuillez saisir des valeurs valide");
+      }
     }
-    else{
-        alert("Veuillez saisir des valeurs valide")
-    }
-   
-    } 
     // Envoient de nos données au local storage
-    addToCart.addEventListener("click", event => {
-        cartItems(productData._id, quantityValue.value, colorValue.value);
-        
+    addToCart.addEventListener("click", (event) => {
+      cartItems(productData._id, quantityValue.value, colorValue.value);
     });
-})
+  });
--- front/js/script.js
+++ front/js/script.js
@@ -1,25 +1,23 @@
 //variables
-
 const url = "http://localhost:3000/api/products/";
+
 const itemsId = document.getElementById("items");
-// On récupère le lien de l'api et on injecte les données dans le HTML.
-fetch (url)
 
-.then( (res) => res.json())
-.then((data) => {
-
-  for (let i = 0 ; i < data.length; i++){
-    itemsId.innerHTML += `
+// On récupère le lien de l'api et on injecte les données dans le HTML pour chaque produit.
+fetch(url)
+  .then((res) => res.json())
+  .then((data) => {
+    for (let i = 0; i < data.length; i++) {
+      itemsId.innerHTML += `
     <a href="./product.html?${data[i]._id}">
     <article>
       <img src="${data[i].imageUrl}" alt="${data[i].altTxt}">
       <h3 class="productName">${data[i].name}</h3>
       <p class="productDescription">${data[i].description}</p>
     </article>
-  </a> `;  
-  };
-})
-.catch(function(err){
-   alert("Une erreur c'est produite")
-})
-
+  </a> `;
+    }
+  })
+  .catch(function (err) {
+    alert("Une erreur c'est produite");
+  });
